937d6fe79e503862df1843e00497f6f01708f723
8codingeek <alishersulimov@gmail.com>
2019-04-18 22:22:28 +0500

    c in img

--- api/app/helpers/setParamsForImage.js
+++ api/app/helpers/setParamsForImage.js
@@ -5,10 +5,12 @@ export default async function setParamsForImage(ctx){
 	const file = ctx.request.files.img;
 
 	if(file.type === 'image/jpeg' || file.type === 'image/png'){
+		console.log("FILENAME: ", file.path);
+		console.log("FILENAME WITH SLICE: ", file.path.slice(-54));
 		return {
 			fileName: file.path.slice(-43), 
 			type : file.type,
-			path : file.path,
+			path : file.path.slice(-54),
 			lastModifiedDate : file.lastModifiedDate 
 		};	
 	}else{
